6c3c221627425eaac91e718e95cc91aba2f15134
wgx0428 <656052658@qq.com>
2017-04-28 11:04:59 +0800

    0

---
+++ WinForms.Study/WinForm.双缓冲实例/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace WinForm.双缓冲实例
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnNormal = new System.Windows.Forms.Button();
+            this.btnCloseNormal = new System.Windows.Forms.Button();
+            this.btnDoubleBuffering = new System.Windows.Forms.Button();
+            this.btnCloseDoubleBuffering = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // btnNormal
+            // 
+            this.btnNormal.Location = new System.Drawing.Point(212, 185);
+            this.btnNormal.Name = "btnNormal";
+            this.btnNormal.Size = new System.Drawing.Size(75, 23);
+            this.btnNormal.TabIndex = 1;
+            this.btnNormal.Text = "普通绘制";
+            this.btnNormal.UseVisualStyleBackColor = true;
+            this.btnNormal.Click += new System.EventHandler(this.btnNormal_Click);
+            // 
+            // btnCloseNormal
+            // 
+            this.btnCloseNormal.Location = new System.Drawing.Point(293, 185);
+            this.btnCloseNormal.Name = "btnCloseNormal";
+            this.btnCloseNormal.Size = new System.Drawing.Size(92, 23);
+            this.btnCloseNormal.TabIndex = 2;
+            this.btnCloseNormal.Text = "关闭普通绘制";
+            this.btnCloseNormal.UseVisualStyleBackColor = true;
+            this.btnCloseNormal.Click += new System.EventHandler(this.btnCloseNormal_Click);
+            // 
+            // btnDoubleBuffering
+            // 
+            this.btnDoubleBuffering.Location = new System.Drawing.Point(212, 245);
+            this.btnDoubleBuffering.Name = "btnDoubleBuffering";
+            this.btnDoubleBuffering.Size = new System.Drawing.Size(75, 23);
+            this.btnDoubleBuffering.TabIndex = 3;
+            this.btnDoubleBuffering.Text = "双缓冲绘制";
+            this.btnDoubleBuffering.UseVisualStyleBackColor = true;
+            this.btnDoubleBuffering.Click += new System.EventHandler(this.btnDoubleBuffering_Click);
+            // 
+            // btnCloseDoubleBuffering
+            // 
+            this.btnCloseDoubleBuffering.Location = new System.Drawing.Point(293, 245);
+            this.btnCloseDoubleBuffering.Name = "btnCloseDoubleBuffering";
+            this.btnCloseDoubleBuffering.Size = new System.Drawing.Size(92, 23);
+            this.btnCloseDoubleBuffering.TabIndex = 4;
+            this.btnCloseDoubleBuffering.Text = "关闭双缓冲绘制";
+            this.btnCloseDoubleBuffering.UseVisualStyleBackColor = true;
+            this.btnCloseDoubleBuffering.Click += new System.EventHandler(this.btnCloseDoubleBuffering_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 287);
+            this.Controls.Add(this.btnCloseDoubleBuffering);
+            this.Controls.Add(this.btnDoubleBuffering);
+            this.Controls.Add(this.btnCloseNormal);
+            this.Controls.Add(this.btnNormal);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnNormal;
+        private System.Windows.Forms.Button btnCloseNormal;
+        private System.Windows.Forms.Button btnDoubleBuffering;
+        private System.Windows.Forms.Button btnCloseDoubleBuffering;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ WinForms.Study/WinForm.双缓冲实例/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WinForm.双缓冲实例
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNormal_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            NormalDraw();
+        }
+
+        private void btnCloseNormal_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        #region 正常绘图
+        /// <summary>
+        /// 正常绘图
+        /// </summary>
+        private void NormalDraw()
+        {
+            DateTime t1 = DateTime.Now;
+            Graphics g = this.CreateGraphics();
+            //g.SmoothingMode = SmoothingMode.HighQuality;//设置显示的质量
+            Brush brush = null;
+
+            bool flag = true;
+            if (flag)
+            {
+                brush = new LinearGradientBrush(new PointF(0.0f, 0.0f), new PointF(700.0f, 300.0f), Color.Red, Color.Blue);
+                flag = false;
+            }
+            else
+            {
+                brush = new LinearGradientBrush(new PointF(0.0f, 0.0f), new PointF(700.0f, 300.0f), Color.Blue, Color.Red);
+                flag = true;
+            }
+            for (int j = 0; j < 60; j++)
+            {
+                for (int i = 0; i < 60; i++)
+                {
+                    g.FillEllipse(brush, i * 10, j * 10, 10, 10);
+                }
+            }
+            DateTime t2 = DateTime.Now;
+            TimeSpan sp = t2 - t1;
+            float per = 1000 / (sp.Milliseconds == 0 ? 1 : sp.Milliseconds);
+            this.label1.Text = "速度：" + per.ToString() + "帧/秒";
+        }
+        #endregion
+
+        #region 启用双缓冲的绘图
+        /// <summary>
+        /// 启用双缓冲的绘图
+        /// </summary>
+        private void EnableDoubleBuffer()
+        {
+            DateTime t1 = DateTime.Now;
+
+            Bitmap bmp = new Bitmap(6000, 6000);    //在内存中建立一块画布
+            Graphics g = Graphics.FromImage(bmp);   //获取这块内存画布的Graphics引用：
+            Brush brush = null;
+
+            bool flag = true;
+            if (flag)
+            {
+                brush = new LinearGradientBrush(new PointF(0.0f, 0.0f), new PointF(700.0f, 300.0f), Color.Red, Color.Blue);
+                flag = false;
+            }
+            else
+            {
+                brush = new LinearGradientBrush(new PointF(0.0f, 0.0f), new PointF(700.0f, 300.0f), Color.Blue, Color.Red);
+                flag = true;
+            }
+            for (int j = 0; j < 600; j++)
+            {
+                for (int i = 0; i < 600; i++)
+                {
+                    //在这块内存画布上绘图
+                    g.FillEllipse(brush, i * 10, j * 10, 10, 10);
+                }
+            }
+
+            //将内存画布画到窗口中
+            this.CreateGraphics().DrawImage(bmp, 0, 0);
+
+            DateTime t2 = DateTime.Now;
+            TimeSpan sp = t2 - t1;
+            float per = 1000 / (sp.Milliseconds == 0 ? 1 : sp.Milliseconds);
+            this.label1.Text = "速度：" + per.ToString() + "帧/秒";
+        }
+
+        #endregion
+
+        private void btnDoubleBuffering_Click(object sender, EventArgs e)
+        {
+            timer2.Start();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            EnableDoubleBuffer();
+        }
+
+        private void btnCloseDoubleBuffering_Click(object sender, EventArgs e)
+        {
+            timer2.Stop();
+        }
+
+    }
+}
